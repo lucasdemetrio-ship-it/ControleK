0c58005c75042c68bcccc0e700a20d15c8085482
Lucas Eduardo Demétrio <lucaseduardodemetrio08@gmail.com>
2026-09-03 16:44:12 -0300

    Corrigir deploy no Vercel

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const path = require('path');
 const apiRouter = require('./routes/api');
 
 const app = express();
+
 app.use(express.json());
 app.use('/api', apiRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+
+if (process.env.VERCEL !== '1') {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
